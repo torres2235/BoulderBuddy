1b091200ebcf5a836df3d567d752650a400ccac1
torres2235 <43655957+torres2235@users.noreply.github.com>
2025-02-27 12:41:04 -0800

    loading in test data

---
+++ data/ClimbImages.js
@@ -0,0 +1,5 @@
+import wall1 from "@/assets/images/climbs/wall1.jpeg";
+import wall2 from "@/assets/images/climbs/wall2.jpeg";
+import wall3 from "@/assets/images/climbs/wall3.jpeg";
+
+export default [wall1, wall2, wall3];
---
+++ data/ClimbItems.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    id: 1,
+    title: "Easy v1",
+    grade: "v1",
+    color: "green",
+    date: "2/1/2025",
+    rating: 4,
+    completed: true,
+    tags: "",
+  },
+  {
+    id: 2,
+    title: "Breazy v2",
+    grade: "v2",
+    color: "blue",
+    date: "2/2/2025",
+    rating: 5,
+    completed: true,
+    tags: "",
+  },
+  {
+    id: 3,
+    title: "tougher v3",
+    grade: "v3",
+    color: "yellow",
+    date: "2/1/2025",
+    rating: 3.5,
+    completed: false,
+    tags: "dyno",
+  },
+];
